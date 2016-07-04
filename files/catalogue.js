bb2f672bd3612917834b0e324f5f2dd87f072fb4
Tobias Neil <brortobiasneil@gmail.com>
2016-07-04 15:27:12 +0200

    Allows appending to existing catalogue

--- files/catalogue.js
+++ files/catalogue.js
@@ -1,7 +1,7 @@
 var jsonfile = require('jsonfile');
 
 
-const createCatalogue = (speeches, cataloguePath) => {
+const createCatalogue = (speeches, cataloguePath, append) => {
     const catalogue = speeches.reduce((cat, speech) => {
         if (!cat.hasOwnProperty(speech.videoId))
             cat[speech.videoId] = {
@@ -18,6 +18,22 @@ const createCatalogue = (speeches, cataloguePath) => {
         cat[speech.videoId].speeches.push(segmentMetadata);
         return cat;
     }, {});
+    if (append) {
+        const existingCat = jsonfile.readFileSync(cataloguePath);
+        existingCat.videoIds.forEach(videoId => {
+           if (!catalogue.hasOwnProperty(videoId)) {
+               catalogue[videoId] = existingCat[videoId];
+           } else {
+               existingCat[videoId].speeches.forEach(existingSpeech => {
+                    const inCat = catalogue[videoId].speeches.some(speech =>
+                        speech.debateTurn === existingSpeech.debateTurn
+                    );
+                    if (!inCat)
+                        catalogue[videoId].speeches.push(existingSpeech);
+               });
+           }
+        });
+    }
     jsonfile.writeFile(cataloguePath, catalogue);
 };
 
