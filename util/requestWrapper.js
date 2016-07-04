d7b74bb6bfa9c53b48847002dd65490c3e0b64a9
Tobias Neil <brortobiasneil@gmail.com>
2016-07-04 15:26:01 +0200

    Added loggin

--- util/requestWrapper.js
+++ util/requestWrapper.js
@@ -36,20 +36,27 @@ const getFromCache = (namespace, documentId, urlFunction, callback) => {
  * due to excesses of requests. If too many requests are sent at once, refused requests will be
  * set on a timeout, specified in the config file in the root folder.
  */
-const get = (req, callback) => {
+const get = (req, callback, isCritical) => {
     request(req, (err, res, body) => {
-        if (err && err.code === 'ECONNRESET') {
-            console.error(`Too many requests sent, waiting ${config.tooManyRequestsTimeout} ms before trying again`);
+        if (err && (err.code === 'ECONNRESET' || err.code === 'ETIMEDOUT' || err.code == 'ECONNREFUSED')) {
+            console.error(`Got error ${err.code} when calling ${req}, waiting ${config.tooManyRequestsTimeout} ms before trying again`);
             setTimeout(() => {
                 get(req, callback)
             }, config.tooManyRequestsTimeout);
         }
         else if (err || res.statusCode !== 200) {
             if (!!res)
-                console.error("Request failed with code", res.statusCode);
+                console.error("Request", req, "failed with code", res.statusCode);
             if (!!err)
-                console.error("Request threw error", err.code);
-            callback(null);
+                console.error("Request", req, "threw error", err.code);
+            if (!!isCritical) {
+                console.error(`Critical request failed ${req}, waiting ${config.tooManyRequestsTimeout} ms before trying again`);
+                setTimeout(() => {
+                    get(req, callback, true)
+                }, config.tooManyRequestsTimeout);
+            } else {
+                callback(null);
+            }
         }
         else {
             callback(JSON.parse(body));
